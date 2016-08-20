e34a0fd529433e0d6544c863983e92cca1156326
bastimeyer <mail@bastimeyer.de>
2016-08-20 09:35:20 +0200

    Add click callback to node-notifier notifications

--- src/app/models/localstorage/Settings.js
+++ src/app/models/localstorage/Settings.js
@@ -148,7 +148,7 @@ export default Model.extend({
 			label: {
 				name: "Notification center",
 				description: "Native notifications on MacOS",
-				notes: "Does not support click events"
+				notes: "Notifications can be configured in your system preferences"
 			}
 		},
 		{
